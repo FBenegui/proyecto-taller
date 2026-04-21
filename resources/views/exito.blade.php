c986892a2eaf08e380798949af2063715ce53486
fabri benegui <fabrizziobenegui28@gmail.com>
2026-04-21 09:57:01 -0300

    cambie una coma

--- resources/views/exito.blade.php
+++ resources/views/exito.blade.php
@@ -6,7 +6,7 @@
 <div class="container mt-5">
     <div class="alert alert-success">
         <p class="lead">
-            Hola <strong>{{ $nombre }}</strong>, qué bueno recibir tu mensaje. Un miembro
+            Hola <strong>{{ $nombre }}</strong>, qué, bueno recibir tu mensaje. Un miembro
             del equipo de ventas se pondrá en contacto con vos al correo: <strong>{{ $email }}</strong> ¡Muchas gracias!
         </p>
     </div>
