3f28924cca02da71924ec9ed1ad1513c64e07734
fabri benegui <fabrizziobenegui28@gmail.com>
2026-04-27 17:56:19 -0300

    contactanos, estilos y mensaje de exito

--- resources/views/contactanos.blade.php
+++ resources/views/contactanos.blade.php
@@ -24,5 +24,26 @@
             <p class="tarjeta-text"><strong>Horario de atención:</strong> Lunes a Viernes de 9:00 a 13:00 - 17:00 a 21:00</p>
         </div>
     </div>
+    <form action="{{ url('/contactanos') }}" method="POST">
+        @csrf
+        <h2> Formulario de contacto</h2>
+    <div class="form-imput">
+        <label for="nombre">Nombre Completo</label>
+        <input type="text" id="nombre" placeholder="Tito Calderon" required name="nombre">
+    </div>
+    <div class="form-imput">
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="tu.email@ejemplo.com" required name="email">
+    </div>
+    <div class="form-imput">
+        <label for="consulta">Consulta</label>
+        <textarea id="consulta" placeholder="Dejanos tu consulta" required name="consulta"></textarea>
+    </div>
+    <div class="form-imput">
+        <button type="submit">Enviar Mensaje</button>
+    </div>
+
+    </form>
+</div>
 
 @endsection
