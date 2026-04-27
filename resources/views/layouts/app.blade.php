7ea3b080f801791d1a7063b07e0387b05ac446f4
fabri benegui <fabrizziobenegui28@gmail.com>
2026-04-27 11:33:30 -0300

    sobre-nosotros y icono de la pagina

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Mi Paginas')</title>
+    <!--icono de la pagina-->
+    <link rel="icon" href="{{ asset('imagenes/icono.png') }}" type="image/png">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
--- resources/views/sobre-nosotros.blade.php
+++ resources/views/sobre-nosotros.blade.php
@@ -4,12 +4,22 @@
 
 @section('content')
 <div class="container mt-5">
-    <div class="card">
-        <div class="card-body">
+    <div class="card" style="width: 100%;">
+        <div class="card-body" style="text-align: left;">
             <h1 class="card-title">Sobre Nosotros</h1>
-            <p>Cebar Club es una compania para la gente a la cual le gusta tomar mates, ofrecemos productos de la mejor calidad para disfrutar de algo tan especial como un mate.</p>
+            <p style="width: 45vw; float: left; text-align: justify;">
+            ¡Hola! Somos <strong>Fabrizzio</strong> y <strong>Valentin</strong>, los creadores detrás de <strong>Cebar Club</strong>.
+            Todo empezó como empiezan las mejores ideas en nuestro país: compartiendo un mate. Entre termo y termo, nos dimos cuenta de que el mate es mucho más que una infusión; es nuestra pausa, nuestro compañero de estudio, el testigo de las mejores charlas y el punto de encuentro con los nuestros.
+            Sin embargo, como buenos fanáticos, siempre buscábamos "ese" mate perfecto. El que se adapta a la mano, el que tiene el diseño ideal, el que te acompaña a todos lados sin fallar. Así fue como pasamos de ser dos amigos tomando mate a dos emprendedores con una misión: crear y elegir mates que estén a la altura de esos momentos.
+            En <strong>Cebar Club</strong> no solo vendemos mates.</p>
+            <img src="https://imgs.search.brave.com/6-0vS5g9PoP0LpOiMWvKv7qAXZIdSAzkNAXc7oHHG8M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMw/OTIwNTA1Mi9waG90/by9lbmpveWluZy1y/aWNoLW1hdGVzLWFu/ZC1tb3VudGFpbi1s/YW5kc2NhcGVzLmpw/Zz9zPTYxMng2MTIm/dz0wJms9MjAmYz1I/c2FwQ0s2bFlUYUJu/S2pIZGtZRmliVjU4/N1JyWENHQmkyYkRa/NTdabEw4PQ" alt="Imagen de cebar club" style="float: right; width: 35%; height: auto; margin-right: 40px;">            
+            <p style="width: 45vw; float: right; margin-right: 10px; text-align: justify;">
+            Buscamos llevarte un pedacito de nuestra tradición con un toque moderno. Cada producto que vas a encontrar en nuestra tienda fue pensado, probado y seleccionado por nosotros mismos, porque somos nuestros clientes más exigentes. Trabajamos con dedicación (¡y mucha yerba de por medio!) para ofrecerte calidad, diseño y esa calidez que solo un buen mate te puede dar.
+            Ya sea que estés buscando tu primer mate, renovando tu equipo o buscando el regalo perfecto, queremos acompañarte.
+            Gracias por sumarte a nuestra ronda y apoyar nuestro emprendimiento. ¡Que nunca te falte un buen mate!</p>
+            <img src="https://imgs.search.brave.com/biOc2wZYPtNVv7y6edc0TPeKiRSgyq5zigxEELEPnCI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hY2Ru/LXVzLm1pdGllbmRh/bnViZS5jb20vc3Rv/cmVzLzAwNi80ODEv/MjA1L2NhdGVnb3Jp/ZXMvZHNjXzAwMjct/NjBiNGM4NWMyNDkx/NDBmZTkxMTc1NTUy/Nzc3NzkwMjEtNDgw/LTAuanBn" alt="Imagen de cebar club" style="float: left; width: 35%; height: auto; margin-left: 30px;">            
+            <h4 style="clear: both; margin-top: 20px; text-align: center;">Muchas gracias, por tenernos en cuenta, Fabrizzio y Valentin</h4>
             
-        </div>
     </div>
 </div>
 @endsection
